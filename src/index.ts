c772112073c2390f771f774186a7920bfdfa1a8d
Jean-Philippe Sirois <jp@veksen.net>
2023-01-31 18:15:02 -0500

    it works

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const { default: antlr4 } = await import("antlr4");
-// import antlr4 from "antlr4";
+// const { default: antlr4 } = await import("antlr4");
+import antlr4 from "antlr4";
 
 const chars = new antlr4.InputStream("1 + 2");
 

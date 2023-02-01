f7d7847c25eb8b1fda04667225dce9f5d1013ca0
Jean-Philippe Sirois <jp@veksen.net>
2023-01-31 19:04:09 -0500

    broke it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // const { default: antlr4 } = await import("antlr4");
 import antlr4 from "antlr4";
+import { startServer } from "./server";
 
 const chars = new antlr4.InputStream("1 + 2");
 
 console.log(chars);
+
+startServer();
 
 export { chars };
